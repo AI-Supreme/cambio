f111544df115b7a76417c45efd72ef3318cc9ac3
Arlindo jose <arlindojosboa@gmail.com>
2021-03-06 12:36:05 +0200

    batabase schema

---
+++ prisma/migrations/20210306103331_create_tables/migration.sql
@@ -0,0 +1,91 @@
+-- CreateTable
+CREATE TABLE "countries" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "corrency_id" INTEGER NOT NULL,
+    "iso_4217" TEXT NOT NULL,
+    "cambio" JSONB NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "currencies" (
+    "id" SERIAL NOT NULL,
+    "currency" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "person_api" BOOLEAN NOT NULL DEFAULT false,
+    "cambio_api" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "people" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "tel2" TEXT,
+    "mae" TEXT,
+    "pai" TEXT,
+    "gender_id" INTEGER NOT NULL,
+    "escolaridade" TEXT,
+    "profissao" TEXT,
+    "nascido" TEXT,
+    "obito" BOOLEAN NOT NULL DEFAULT false,
+    "codigo_postal" INTEGER,
+    "provincia" TEXT,
+    "cidade" TEXT,
+    "bairro" TEXT,
+    "quarteirao" INTEGER,
+    "casa" INTEGER,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "genders" (
+    "id" SERIAL NOT NULL,
+    "gender" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "countries.iso_4217_unique" ON "countries"("iso_4217");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users.email_unique" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "people.email_unique" ON "people"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "genders.gender_unique" ON "genders"("gender");
+
+-- AddForeignKey
+ALTER TABLE "countries" ADD FOREIGN KEY ("corrency_id") REFERENCES "currencies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "people" ADD FOREIGN KEY ("gender_id") REFERENCES "genders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "people" ADD FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
